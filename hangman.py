58e37941436e1574eab0f37c53d4cf094cd4818e
Amirtha <amirtha4501@gmail.com>
2020-05-07 09:02:49 +0530

    Hangman game using python

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rem_letters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in rem_letters:
+            cind = rem_letters.index(char)
+            board[cind] = char
+            rem_letters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+
+word_list = ["camel", 'cat', 'puppy','cow', 'goat', 'zebra', 'lion', 'bear', 'dog']
+my_word = random.randint(0, len(word_list))
+hangman(word_list[my_word])   
